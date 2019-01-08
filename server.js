4846e3adec02ef7ae3ebd87dfdc1a899999fd011
Daniel Paschal <daniel.paschal@learningfuze.com>
2019-01-07 17:16:44 -0800

    added basic endpoint

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const server = express();
 
 server.use ( express.static( __dirname + '/documentroot'));
 
+server.post('/login', (request, response)=>{
+	response.send('yes');
+})
+
 server.listen( 4000, ()=>{
 	console.log('carrier has arrived');
 });
