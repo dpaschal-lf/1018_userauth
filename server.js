908da97291f962c720d730109e27c3ea457bc80d
Daniel Paschal <daniel.paschal@learningfuze.com>
2019-01-07 17:44:00 -0800

    added initial database connection and sha1 encryption, as well as pull data from body

--- server.js
+++ server.js
@@ -3,15 +3,22 @@
 const express = require('express');
 const mysql = require('mysql');
 const creds = require('./mysql_cred.js');
-
+const sha1 = require('sha1');
 const db = mysql.createConnection( creds );
 
 const server = express();
+server.use(express.urlencoded({ extended: false }))
+server.use( express.json() );
 
 server.use ( express.static( __dirname + '/documentroot'));
 
 server.post('/login', (request, response)=>{
-	response.send('yes');
+	db.connect(()=>{
+		const username = request.body.user;
+		const password = sha1(request.body.pass);
+		delete request.body.pass;
+		console.log(username, password);
+	})
 })
 
 server.listen( 4000, ()=>{
