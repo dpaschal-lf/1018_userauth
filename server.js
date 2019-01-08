f68707d22b036200e2454a7156cf463e5f60ca0f
Daniel Paschal <daniel.paschal@learningfuze.com>
2019-01-07 17:01:38 -0800

    added express base code

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-server.js
+
+
+const express = require('express');
+
+const server = express();
+
+server.use ( express.static( __dirname + '/documentroot'));
+
+server.listen( 4000, ()=>{
+	console.log('carrier has arrived');
+});
